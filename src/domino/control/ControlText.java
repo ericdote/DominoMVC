cfef9268163a4c464f0594cf249f47ed7ae3f3b7
ericdote <pokemon13>
2016-11-17 17:06:27 +0100

    Faltan comprovacion de errores Eric Dote

--- src/domino/control/ControlText.java
+++ src/domino/control/ControlText.java
@@ -7,19 +7,26 @@ import domino.model.Torn;
 import domino.vista.VistaText;
 
 public class ControlText {
-
+    //Variables que utilitzarem durant tota la clase
     private final Joc joc;
     private final VistaText vt;
     private Torn torn;
     private Jugador jug;
     private Fitxa fitxa;
-
+    /**
+     * Inicialitzem el constructor amb el numero de jugadors, les fitxes, i quantes fitxes per jugador
+     */
     public ControlText() {
         this.joc = new Joc(4, 28, 7);
         this.vt = new VistaText();
         this.torn = new Torn(joc);
     }
-
+    /**
+     * Aquest metode inicialitza el joc, creant els jugadors, fent el torn inicial i comenzant al seguent torn
+     * Un cop fet aixo fa un bucle que durará fins que el metode isFinalitzat indiqui que acaba el joc.
+     * Dins del bucle es mostren les dades del torn, les fitxes jugades, les fitxes del jugador, el menu i actualitza l'estat
+     * Un cop acabar mostra el guanyador.
+     */
     public void inici() {
         joc.iniciar(new String[]{"J1", "J2", "J3", "J4"});
         torn.inicial();
@@ -40,7 +47,11 @@ public class ControlText {
         } while (!joc.isFinalitzat());
         vt.imprimirGuanyador(joc.trobarGuanyador());
     }
-
+    /**
+     * Mostra el menu i rep l'opcio que vol escogir l'usuari.
+     * En els 2 primers cassos fa comprovacions i en cas de poder colocar surt.
+     * En el cas 3 passa torn.
+     */
     public void controlMenu() {
         int opcio = vt.menu();
 
@@ -69,7 +80,11 @@ public class ControlText {
                 break;
         }
     }
-
+    /**
+     * Aquest metode agafa la posicio de la fitxa, la fitxa que es i el seu extrem. 
+     * Un cop agafat tot aixo realitza una comprovacio per probar a colocar la fitxa.
+     * @return la comprobacio.
+     */
     public boolean colocarFitxa() {
         boolean comprobacio, extrem;
         Fitxa f;
@@ -82,7 +97,11 @@ public class ControlText {
 
         return comprobacio;
     }
-
+    /**
+     * Aquest metode agafa les 2 posicions (cadascuna d'una fitxa), la fitxa que es i el seu extrem.
+     * Un cop agafat tot realitza una comprobacio i les coloca.
+     * @return la comprobacio.
+     */
     public boolean colocarDobles() {
         boolean comprobacio, extrem1, extrem2;
         int posicio1, posicio2;
@@ -101,7 +120,12 @@ public class ControlText {
         comprobacio = torn.colocarDosDobles(f1, extrem1, f2, extrem2);
         return comprobacio;
     }
-
-
-
+    
+    public boolean comprovarPosicioFitxa(int ficha) {
+        boolean comprovacio = false;
+                if (ficha > 0 && ficha <= joc.jugadors[joc.getTorn()].getFitxes().size()) {
+                    comprovacio = true;
+                }
+        return comprovacio;
+    }
 }
--- src/domino/vista/VistaText.java
+++ src/domino/vista/VistaText.java
@@ -13,11 +13,24 @@ public class VistaText {
     private final Scanner lector = new Scanner(System.in);
     private Jugador jug;
     private Joc joc;
+    private ControlText ct = new ControlText();
 
+    /**
+     * Dades que es mostraran al principi de cada torn
+     *
+     * @param torn torn actual
+     * @param jugador jugador actual
+     */
     public void dadesTorn(int torn, Jugador jugador) {
         System.out.println("TORN: " + torn + "         " + jugador.getNom());
     }
 
+    /**
+     * Mostra les fitxes jugades durant la partida
+     *
+     * @param fitxesJugades arrayDeque que te les fitxes que s'han jugat durant
+     * la partida
+     */
     public void fitxesJugades(Deque<Fitxa> fitxesJugades) {
 
         StringBuilder sb = new StringBuilder();
@@ -32,12 +45,24 @@ public class VistaText {
         System.out.println(sb);
     }
 
+    /**
+     * Mostra les fitxes del jugador
+     *
+     * @param fitxes ArrayList que conte les fitxes del jugador
+     */
     public void fitxesJugador(List<Fitxa> fitxes) {
         for (int i = 0; i < fitxes.size(); i++) {
             System.out.print(" " + fitxes.get(i).valors[0] + ":" + fitxes.get(i).valors[1] + " ");
         }
     }
 
+    /**
+     * Metode que mostra el menu per escogir una opcio La opcio es escogida amb
+     * el metode que comprova que entri un valor correcte
+     *
+     * @return la opcio escogida per en ControlText donar pas a la accio
+     * seleccionada
+     */
     public int menu() {
         System.out.println("Escogeix la jugada: \n"
                 + "1- Col·locar 1 fitxa \n"
@@ -47,24 +72,41 @@ public class VistaText {
         return opcio;
     }
 
+    /**
+     * Mostra per pantalla un sout on es demana la posicio de la fitxa La fitxa
+     * s'escogeix amb comprovarPosicioFitxa.
+     *
+     * @param fitxas ArrayList de fitxes del jugador
+     * @return Torna la posicio de la fitxa seleccionada pel jugador -1 per
+     * indicar la posicio en l'array.
+     */
     public int fitxaEscogida(List<Fitxa> fitxas) {
         int fitxa;
         do {
             System.out.print("Introdueix la posicio de la fitxa que vols escogir: ");
-            fitxa = comprovarPosicioFitxa();
-            break;
-        } while (comprovarFitxa(fitxa, fitxas.size()));
+            fitxa = comprovarValorPosicio();
+            if (!ct.comprovarPosicioFitxa(fitxa)) {
+                System.out.print("No es valor correcte, introduiu un altre: ");
+            }
+        } while (!comprovarFitxa(fitxa, fitxas.size()));
         return fitxa - 1;
     }
 
     public boolean comprovarFitxa(int fitxa, int fitxesJugador) {
-        boolean comprovar = true;
+        boolean comprovar = false;
         if (fitxa > 0 && fitxesJugador <= 7) {
-            comprovar = false;
+            comprovar = true;
         }
         return comprovar;
     }
 
+    /**
+     * Demana el costat on volem col·locar la fitxa. I fa una comprovacio de que
+     * nomes es pugui col·locar la fitxa a esquerra o dreta
+     *
+     * @return Torna un boolean dient si es pot o no colocar la fitxa al costat
+     * escogit.
+     */
     public boolean costatFitxa() {
         boolean costat = false;
         String orientacio;
@@ -81,10 +123,21 @@ public class VistaText {
         return costat;
     }
 
+    /**
+     * Imprimeix el guanyador
+     *
+     * @param guanyador li entra per parametre el nom del guanyador
+     */
     public void imprimirGuanyador(Jugador guanyador) {
         System.out.println("El guanyador es: " + guanyador.getNom());
     }
 
+    /**
+     * Metode fet per comprovar que entra un numero correcte y no una lletra o
+     * un numero invalid.
+     *
+     * @return Torna el numero de la opcio
+     */
     public int comprovacioMenu() {
         int opcio = 0;
         do {
@@ -103,6 +156,13 @@ public class VistaText {
         return opcio;
     }
 
+    /**
+     * Metode per comprovar que la orientacio que entra es una orientacio
+     * correcta, es a dir, e/E/d/D i no cap altre
+     *
+     * @param orientacio
+     * @return
+     */
     public boolean comprovarCostat(String orientacio) {
         boolean comprovacio = true;
         if (orientacio.equals("e") || orientacio.equals("E") || orientacio.equals("d") || orientacio.equals("D")) {
@@ -111,21 +171,14 @@ public class VistaText {
         return comprovacio;
     }
 
-    public int comprovarPosicioFitxa() {
-        int fitxa;
-        do {
-            if (lector.hasNextInt()) {
-                fitxa = lector.nextInt();
-                if (fitxa > 0 && fitxa <= jug.getFitxes().size()) {
-                    break;
-                } else {
-                    System.out.print("Introdueix un numero valid: ");
-                }
-            } else {
-                System.out.print("Ha de ser un numero: ");
-                lector.nextLine();
-            }
-        } while (true);
-        return fitxa;
+    public int comprovarValorPosicio() {
+        int posicio = 0;
+        if (lector.hasNextInt()) {
+            posicio = lector.nextInt();
+        } else {
+            System.out.println("Ha de ser un numero la posicio");
+        }
+        return posicio;
     }
+
 }
